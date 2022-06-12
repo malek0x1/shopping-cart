4d3143d730c5bb778d0830c94c034029168b90b3
Malek Mneimneh <106251533+malek0x1@users.noreply.github.com>
2022-06-13 00:17:28 +0300

    Delete gulp-basics directory

--- gulp-basics/gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-
-const gulpSass = require('gulp-sass');
-const sass = gulpSass(require('sass'));
-
-
-gulp.task("sass", async function () {
-    gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("css"))
-})
